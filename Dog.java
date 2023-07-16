cbb16bb43c80fda82846c85d8afd370cd961b28b
eparramoure <elise.parramoure@snhu.edu>
2023-07-16 07:47:35 -0500

    PetStay
    collect and retain relevant information on the dogs/cats that will be staying at the boarding facility

---
+++ Dog.java
@@ -0,0 +1,109 @@
+/**
+ * 
+ */
+
+/**
+ * @author eliseparramou_snhu
+ *
+ */
+public class Dog {
+	private String petType;
+	private String petName;
+	private int petAge;
+	private int dogSpaces;
+	private int catSpaces;
+	private double amountDue;
+	private int daysStay;
+	private int dogWeight;
+	private String.valueOf(grooming);
+	private int dogSpaceNumber;
+	
+public Dog(String petType, String petName, int petAge, int dogSpaces, int catSpaces, double amountDue, int daysStay, int dogWeight, int groomingPrice, int dogSpaceNumber) {
+this.petType = petType;
+this.petName = petName;
+this.amountDue = amountDue;
+this.catSpaces = catSpaces;
+this.daysStay = daysStay;
+this.dogSpaces = dogSpaces;
+this.petAge = petAge;
+this.dogWeight = dogWeight;
+this.groomingPrice = groomingPrice;
+this.dogSpaceNumber = dogSpaceNumber;
+}
+public String getPetName(){
+	return petName;
+}
+public void setPetName(String petName) {
+	this.petName = petName;
+}
+public String getPetType() {
+	return petType;
+}
+public void setPetType(String petType) {
+	this.petType = petType;
+}
+public int getPetAge() {
+	return petAge;
+}
+public void setPetAge(int petAge) {
+	this.petAge = petAge;
+}
+public int getDogSpaces() {
+	return dogSpaces;
+}
+public void setDogSpaces(int dogSpaces) {
+	this.dogSpaces = dogSpaces;
+}
+public int getCatSpaces() {
+	return catSpaces;
+}
+public void setCatSpaces(int catSpaces) {
+	this.catSpaces = catSpaces;
+}
+public double getAmountDue() {
+	return amountDue;
+}
+public void setAmountDue(double amountDue) {
+	this.amountDue = amountDue;
+}
+public int getDaysStay() {
+	return daysStay;
+}
+public void setDaysStay(int daysStay) {
+	this.daysStay = daysStay;
+}
+public int getDogWeight() {
+	return dogWeight;
+}
+public void setDogWeight(int dogWeight) {
+	this.dogWeight = dogWeight;
+}
+public int getGrooming {
+	return grooming;
+}
+public void setGrooming = grooming {
+	this.grooming = grooming;
+}
+public int getDogSpaceNumber() {
+	return dogSpaceNumber;
+}
+public void setDogSpaceNumber(int dogSpaceNumber) {
+	this.dogSpaceNumber = dogSpaceNumber;
+}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
+args) {
+		
+		
+
+	}
+
+}
